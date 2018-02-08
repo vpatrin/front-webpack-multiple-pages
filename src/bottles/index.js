0b338e1f2f4070d0af880be355c9c46559b501e1
Julien Blatecky <julien.blatecky@creatiwity.net>
2018-02-08 22:45:17 +0100

    Add Javascript examples

--- src/bottles/index.js
+++ src/bottles/index.js
@@ -1,3 +1,11 @@
+import $ from "jquery";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 console.log("Bottles");
+
+// DOM manipulation
+$("#bottles-listing").append(`
+  <tr>
+    <td>Bottle1</td>
+  </tr>
+  `);
--- src/login/index.js
+++ src/login/index.js
@@ -1,3 +1,21 @@
+import $ from "jquery";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 console.log("Login");
+
+// Ajax call
+$.ajax({
+  url: "http://localhost:3131"
+  data: {
+    login: "",
+    password: "",
+  },
+  method: "GET",
+  dataType: "json",
+  error: (request, status, error) => {
+    // Treat error
+  },
+  success: (data, status, request) => {
+
+  }
+});
